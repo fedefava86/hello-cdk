c35d7a2c3f70414186d5b3a30b5821f833ab8b89
Federico Favaro <fedefava86@gmail.com>
2020-11-09 00:34:56 +0100

    First commit

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,4 +1,7 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_s3 as s3,
+    core
+)
 
 
 class HelloCdkStack(core.Stack):
@@ -6,4 +9,4 @@ class HelloCdkStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        bucket = s3.Bucket(self, "FFBucket", versioned=True, removal_policy=core.RemovalPolicy.DESTROY)
